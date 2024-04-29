d04ea76e8fbe59468c4214b2f240d9c184e79d20
Gabriel M. Tello <gabriel.tellocruz@cbrands.com>
2024-04-28 23:55:16 -0600

    Divide and conquer

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,33 @@
 "use strict";
 
-// Print all entries, across all of the *async* sources, in chronological order.
+// Print all entries, across all of the sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
-};
+//Divide and Conquer approach, merge left to right arrays picking the oldest one first
+async function mergeAndSort(leftSource, rightSource) {
+    let leftValue = leftSource?.last || leftSource;
+    let rightValue = rightSource?.last;
+    let mergedLog = [];
+
+    while (leftValue || rightValue){
+      if ((!rightValue) || (leftValue && (leftValue.date > rightValue.date))) {
+        leftValue && mergedLog.push(leftValue);
+        leftValue = await leftSource.popAsync();
+      }
+      else if(rightValue){
+        rightValue && mergedLog.push(rightValue);
+        rightValue = await rightSource.popAsync();
+      }
+    }
+
+    return mergedLog;
+}
+ 
+async function logger(logSources, printer){ 
+    while (logSources.length > 1) {  
+      logSources[0] = await mergeAndSort(logSources.shift(), logSources[0])
+    }
+ 
+    logSources[0].forEach(log => log && log.date && printer.print(log))
+}
+
+module.exports = logger;
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,6 +2,32 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
-};
+//Divide and Conquer approach, merge left to right arrays picking the oldest one first
+function mergeAndSort(leftSource, rightSource) {
+    let leftValue = leftSource?.last || leftSource;
+    let rightValue = rightSource?.last;
+    let mergedLog = [];
+
+    while (leftValue || rightValue){
+      if ((!rightValue) || (leftValue && (leftValue.date > rightValue.date))) {
+        leftValue && mergedLog.push(leftValue);
+        leftValue = leftSource.pop();
+      }
+      else if(rightValue){
+        rightValue && mergedLog.push(rightValue);
+        rightValue = rightSource.pop();
+      }
+    }
+
+    return mergedLog;
+}
+ 
+function logger(logSources, printer){ 
+    while (logSources.length > 1) {  
+      logSources[0] = mergeAndSort(logSources.shift(), logSources[0])
+    }
+ 
+    logSources[0].forEach(log => log && log.date && printer.print(log))
+}
+
+module.exports = logger;
